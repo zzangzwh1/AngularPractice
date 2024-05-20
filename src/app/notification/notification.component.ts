2286a6ed3554b8020d92d2000c7b9601c59ad84d
Wonhyuk Cho <wcho2@nait.ca>
2024-05-20 16:26:20 -0600

    GIT ADD

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -3,11 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-notification',
  template: `<div class="alert alert-success" [hidden]="displayNotification"> 
-  <p>This website uses cookies to provide bettwe user experience. </p> 
+  This website uses cookies to provide bettwe user experience.
+  <div class="close"><div class="btn" (click)="closeNotification()">X</div></div>
   </div>`,
- styles:["div{margin:10px 0px; padding: 10px 20px;  text-align:center;}","p{font-size:14px;}"]
+ styles:["div{margin:10px 0px; padding: 10px 20px;  text-align:center;}","p{font-size:14px;}",".close{float:right; margin-top:-30px;}"]
 })
 export class NotificationComponent {
   displayNotification : boolean = false;
 
+  closeNotification(){
+    this.displayNotification=true;
+  
+  }
+
 }
+
